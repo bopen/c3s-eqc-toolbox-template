fea2ede2e00f49c682bacf03e7598016f223a521
Mattia Almansi <m.almansi@bopen.eu>
2023-03-03 13:04:52 +0100

    add VM script

---
+++ scripts/jupyter_server.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""Start jupyter servers for EQC users on the VM."""
+import getpass
+import logging
+import pathlib
+import subprocess
+import urllib
+
+logging.basicConfig(level=logging.INFO)
+
+wd_path = pathlib.Path.cwd()
+username = wd_path.name
+log = wd_path / "jupyter.log"
+
+expected_parent = pathlib.Path(f"/data/{getpass.getuser()}")
+if wd_path.parent != expected_parent or len(wd_path.name.split("_")) != 2:
+    expected_dir = expected_parent / "lastname_firstname"
+    raise ValueError(f"Working directory must be {expected_dir}")
+
+
+def get_jupyter_server_url(wd_path):
+    cmd = ["jupyter", "notebook", "list"]
+    proc = subprocess.run(cmd, capture_output=True, check=True, text=True)
+    for line in proc.stdout.splitlines()[1:]:
+        jupyter_url, jupyter_dir = line.split(" :: ")
+        if pathlib.Path(jupyter_dir) == wd_path:
+            return urllib.parse.urlparse(jupyter_url)
+    return None
+
+
+def start_jupyter_server(log):
+    cmd = f"jupyter notebook --no-browser > {log!s} 2>&1 &"
+    subprocess.run(cmd, check=True, shell=True)
+    jupyter_url = None
+    while True:
+        jupyter_url = get_jupyter_server_url(wd_path)
+        if jupyter_url:
+            return jupyter_url
+
+
+jupyter_url = get_jupyter_server_url(wd_path)
+if jupyter_url is None:
+    jupyter_url = start_jupyter_server(log)
+
+msg = f"""Serving notebooks from local directory: {wd_path}
+Jupyter Notebook is running at:
+{jupyter_url.geturl()}
+
+To access the notebooks:
+    1. Go to your local machine and paste this command:
+            ssh -N -f -L localhost:5678:{jupyter_url.netloc} eqcuser@136.156.154.152
+    2. Open a browser and paste this URL:
+            http://localhost:5678/?{jupyter_url.query}
+
+To stop the server use this command:
+    jupyter notebook stop {jupyter_url.netloc.split(":")[-1]}
+"""
+logging.info(msg)
