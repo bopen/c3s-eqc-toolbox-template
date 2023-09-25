8768f529bee2f23d6b2beb64cc6c296484647f78
Mattia Almansi <m.almansi@bopen.eu>
2023-09-25 10:14:00 +0200

    add script to get issue stats

---
+++ scripts/issues_stats.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Get statistics of issues."""
+
+import pandas as pd
+from github import Github
+
+filename = input("Insert file name: ")
+token = input("Insert token: ")
+g = Github(login_or_token=token)
+repo = g.get_repo("bopen/c3s-eqc-toolbox-template")
+columns = ["number", "title", "created_at", "closed_at"]
+data = {column: [] for column in columns}
+for issue in repo.get_issues(state="all"):
+    if issue.pull_request:
+        continue
+    for column in columns:
+        data[column].append(getattr(issue, column))
+df = pd.DataFrame.from_dict(data)
+df.to_csv(filename, index=False)
